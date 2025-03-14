96c34a9f8e4f80b6770fc62a2ef873b4723f419c
Linh Pham <linhmaipham@usf.edu>
2025-03-14 15:25:15 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,8 @@
+// Task 1: Creating the Base Structure
+
+// Select the riskDashboard container
+const riskDashboard = document.getElementById("riskDashboard");
+
+// Print message to the console
+console.log("Risk Dashboard Loaded");
+
